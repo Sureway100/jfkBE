7eb4c1bf148b7f4ac4f4c75d99a8865efa3b946b
nwachukwu david <Nwachukwudavid100@gmail.com>
2025-12-31 14:50:08 +0100

    Refactor settings.py to use environment variables
    Updated settings to use environment variables for SECRET_KEY, DEBUG, ALLOWED_HOSTS, and CORS_ALLOWED_ORIGINS.

--- BioData/settings.py
+++ BioData/settings.py
@@ -19,12 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
@@ -58,6 +60,13 @@ CORS_ALLOW_ALL_ORIGINS = False
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
 # ]
+
+CORS_ALLOWED_ORIGINS = os.environ.get(
+    'CORS_ALLOWED_ORIGINS',
+    'http://localhost:3000'
+).split(',')
+
+
 ROOT_URLCONF = "BioData.urls"
 
 REST_FRAMEWORK = {
